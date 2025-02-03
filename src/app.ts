e3dcaff529e57684d811a25a1de31ae29bb98c08
seoyoungKim <seoyoungkim003@gmail.com>
2025-02-03 21:37:21 +0900

    interface basic

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+interface Greetable {
+    name: string,
+    greet(phrase: string) : void;
+}
+class Person implements Greetable {
+    name: string;
+    age = 20;
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet(phrase: string): void {
+        console.log(phrase + ' ' + this.name)
+    }
+}
+let user1: Greetable;
+
+user1 = new Person('max');
+
+user1.greet("Hello")
